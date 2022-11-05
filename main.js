bae0dde49c25a7ce198d1f8e853ebeee6cc77e46
Mazinho <mazinho.github@gmail.com>
2022-11-04 21:41:37 -0300

    Adicionado partidas até o grupo H

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML = 
-    createCard("24/11", "quinta", createMatch("brazil", "16:00", "serbia") + createGame("portugal", "13:00", "uruguay")) +
-    createCard("28/11", "segunda", createMatch("switzerland", "13:00", "brazil")) +
-    createCard("01/11", "sexta", createMatch("brazil", "16:00", "cameroon")) +
-
-    createCard("24/11", "terça", createMatch("spain", "13:00", "france") + createGame("germany", "16:00", "japan"))
+    createCard("20/11", "domingo", createMatch("qatar", "13:00", "ecuador")) +
+    createCard("21/11", "segunda", createMatch("england", "10:00", "iran") + createMatch("senegal", "13:00", "netherlands") + createMatch("united states", "16:00", "wales")) +
+    createCard("22/11", "terca", createMatch("argentina", "07:00", "saudi arabia") + createMatch("denmark", "10:00", "tunisia") + createMatch("mexico", "13:00", "poland") + createMatch("france", "16:00", "australia")) +
+    createCard("23/11", "quarta", createMatch("morocco", "07:00", "croatia") + createMatch("germany", "10:00", "japan") + createMatch("spain", "13:00", "costa rica") + createMatch("belgium", "16:00", "canada")) +
+    createCard("24/11", "", createMatch("switzerland", "07:00", "cameroon") + createMatch("uruguay", "10:00", "south korea") + createMatch("portugal", "13:00", "ghana") + createMatch("brazil", "16:00", "serbia"))
